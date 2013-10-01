21c6129e1a0332ff3e2aa0610c9fa15c105ad222
Yanina Aular <yanina.aular@vauxoo.com>
2013-10-01 02:40:09 -0430

    mejoras mínimas

--- tis.cpp
+++ tis.cpp
@@ -8,10 +8,8 @@ using namespace std;
 
 int main(){
     string input_line;
-    string ini, ejem;
-    int band = 1;
-    
     string a,b;
+    int band = 1;
     stack<string> pila;
 
     while(band){
@@ -21,17 +19,16 @@ int main(){
             band = 0;
         }
         else{
-            ejem = input_line.substr( input_line.find(":") + 1 );      
-            ini = input_line.substr( 0, input_line.find(":") +1 );
-            for(int i = ejem.size()-1; i > -1; i--){
-                if( isalpha( ejem.at(i) ) ){ 
-                    char c = ejem.at(i);
+            cout << input_line.substr( 0, input_line.find(":") +1 ) << ' ';
+            input_line = input_line.substr( input_line.find(":") + 1 );      
+            for(int i = input_line.size()-1; i > -1; i--){
+                if( isalpha( input_line.at(i) ) ){ 
                     stringstream ss; string s;
-                    ss << c; ss >> s;
+                    ss << input_line.at(i); ss >> s;
                     pila.push( s );
                 }
                 else{
-                    if( ejem.at(i) == '&' ){
+                    if( input_line.at(i) == '&' ){
                        a = pila.top(); pila.pop();
                        b = pila.top(); pila.pop();
                        if( a.at(0) == '(' ){
@@ -39,14 +36,14 @@ int main(){
                        }
                        pila.push( "(" + a + ", " + b + ")" );
                     }
-                    if( ejem.at(i) == '@' ){
+                    if( input_line.at(i) == '@' ){
                        a = pila.top(); pila.pop();
                        b = pila.top(); pila.pop();
                        pila.push( a + " -> " + b );
                     }
                 }
             }
-            cout << ini << ' '<< pila.top() << endl;
+            cout << pila.top() << endl;
         }
     }
     return 0;
